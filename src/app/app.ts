ee81749c3c3a05923c62cc1c5a712e1d006db459
Marina Gromova <krapiva87sstu@mail.ru>
2025-11-11 20:28:36 +0300

    6.0 profile get запрос

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,25 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+import { JsonPipe } from '@angular/common';
 import { ProfileCard } from './common-ui/profile-card/profile-card';
+import { ProfileInter } from './data/interfaces/profile.interface';
+import { Profile } from './data/services/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileCard],
+  imports: [ProfileCard, JsonPipe],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('project--social-networks');
+  profileService = inject(Profile);
+
+  profiles: ProfileInter[] = [];
+
+  constructor() {
+    this.profileService.getTestAccounts().subscribe((val) => {
+      this.profiles = val;
+    });
+  }
 }
---
+++ src/app/data/services/profile.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { ProfileInter } from './../interfaces/profile.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Profile {
+  http = inject(HttpClient);
+  baseApiUrl = 'https://icherniakov.ru/yt-course/';
+
+  getTestAccounts() {
+    return this.http.get<ProfileInter[]>(`${this.baseApiUrl}account/test_accounts`);
+  }
+}
