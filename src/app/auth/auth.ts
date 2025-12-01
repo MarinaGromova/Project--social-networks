7fb1cc926e1c022ba2b67bbac8903fb43e7cdb2a
Marina Gromova <krapiva87sstu@mail.ru>
2025-12-01 16:43:49 +0300

    15.00 @ts-ignore

--- src/app/auth/auth.ts
+++ src/app/auth/auth.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class Auth {
-  
+export class AuthService {
+  http = inject(HttpClient);
+  baseApiUrl = 'https://icherniakov.ru/yt-course/auth/';
+
+  login(payload: { username: string; password: string }) {
+    return this.http.post(`${this.baseApiUrl}token`, payload);
+  }
 }
